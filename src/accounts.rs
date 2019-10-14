11fc9f135b10361b14bf1a1b974d1b06e93e9e76
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2019-10-14 03:46:44 +0200

    :art: Run cargo fmt

--- src/accounts.rs
+++ src/accounts.rs
@@ -1,17 +1,17 @@
 // The MIT License
-// 
+//
 // Copyright (c) 2019 Alexandre BOUTHINON
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -43,7 +43,7 @@ impl Account {
     pub fn new(url: &Url) -> Result<Account, failure::Error> {
         // Extract username from URI
         let mut username: String = percent_decode_str(&url.path()[1..])
-            .decode_utf8()? 
+            .decode_utf8()?
             .to_string();
         let useful_beg = &username.find(':').unwrap_or(username.len()) + 1;
         username = username.drain(useful_beg..).collect();
@@ -93,7 +93,11 @@ pub fn pretty_display(accounts: &Vec<Account>) -> String {
             Cell::new(&account.username),
         ]));
     }
-    format!("[+] {} Account[s] found:\n{}", &accounts.len(), table.to_string())
+    format!(
+        "[+] {} Account[s] found:\n{}",
+        &accounts.len(),
+        table.to_string()
+    )
 }
 
 mod tests {
--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,17 @@
 // The MIT License
-// 
+//
 // Copyright (c) 2019 Alexandre BOUTHINON
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,10 +22,11 @@
 
 use super::accounts;
 
+use clipboard::x11_clipboard::{Primary, X11ClipboardContext};
+use clipboard::ClipboardProvider;
+use failure::format_err;
 use structopt::StructOpt;
 use url::Url;
-use failure::format_err;
-use clipboard::{ClipboardContext, ClipboardProvider};
 
 use std::io::{BufRead, BufReader, Read};
 
@@ -52,14 +53,14 @@ pub fn parse_backup<B: Read>(
 }
 
 pub fn send_to_clipboard(value: &str) -> Result<(), failure::Error> {
-    let mut ctx: ClipboardContext = match ClipboardProvider::new() {
+    let mut ctx: X11ClipboardContext<Primary> = match ClipboardProvider::new() {
         Ok(c) => c,
-        Err(err) => return Err(format_err!("{}", err))
+        Err(err) => return Err(format_err!("{}", err)),
     };
 
     match ctx.set_contents(value.to_owned()) {
         Ok(_) => Ok(()),
-        Err(err) => Err(format_err!("{}", err))
+        Err(err) => Err(format_err!("{}", err)),
     }
 }
 
@@ -84,8 +85,7 @@ mod tests {
 
     #[test]
     fn parse_backup_err() {
-        let backup = "NOT_REALLY_AN_URI"
-            .as_bytes();
+        let backup = "NOT_REALLY_AN_URI".as_bytes();
 
         let accounts = parse_backup(backup);
         assert!(accounts.is_err());
